ba60d3c0f3c79dff09a236717f0d421ab594d060
Alexandre Salomé <git@pub.salome.fr>
2025-12-09 13:23:22 +0100

    fetch projects

--- commands/fleet.go
+++ commands/fleet.go
@@ -18,18 +18,40 @@ func newFleetCommand(cnf *config.Config) *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			manager := fleet.NewFleetManager(cnf, cmd)
 
+			fmt.Fprint(cmd.OutOrStdout(), "Authentication...")
+
 			// 1. Verify that the user is authenticated
 			isAuthenticated, email, err := manager.Authentication()
 			if err != nil {
+				fmt.Fprintln(cmd.OutOrStdout(), " failed (internal error)")
 				exitWithError(err)
 			}
 
 			if !isAuthenticated {
-				fmt.Fprintln(cmd.OutOrStdout(), "You are not logged in to Upsun. Please log in to continue.")
+				fmt.Fprintln(cmd.OutOrStdout(), " failed (not authenticated)")
+				fmt.Fprintln(cmd.OutOrStdout(), "\n- You are not logged into Upsun.\n- Please run `upsun login`.")
 				os.Exit(1)
 			}
 
-			fmt.Fprintf(cmd.OutOrStdout(), "Authenticated as %s\n", email)
+			fmt.Fprintln(cmd.OutOrStdout(), "OK")
+			fmt.Fprintf(cmd.OutOrStdout(), "- Current user: %s\n", email)
+
+			// 2. Fetch the list of projects
+			fmt.Fprint(cmd.OutOrStdout(), "Fetching projects... ")
+
+			projects, err := manager.Project.List()
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStdout(), "failed")
+				exitWithError(err)
+			}
+
+			fmt.Fprintln(cmd.OutOrStdout(), "OK")
+			fmt.Fprintf(cmd.OutOrStdout(), "- Found %d projects\n", len(projects))
+
+			// 3. Display the list of projects
+			for _, project := range projects {
+				fmt.Fprintf(cmd.OutOrStdout(), "  - %s (ID: %s)\n", project.ProjectTitle, project.ProjectID)
+			}
 		},
 	}
 
--- internal/fleet/manager.go
+++ internal/fleet/manager.go
@@ -2,6 +2,8 @@ package fleet
 
 import (
 	"bytes"
+	"encoding/csv"
+	"io"
 
 	"github.com/spf13/cobra"
 
@@ -9,16 +11,26 @@ import (
 	"github.com/platformsh/cli/internal/legacy"
 )
 
+//
+// Fleet manager
+//
+
 type FleetManager struct {
 	config  *config.Config
 	command *cobra.Command
+
+	Project *ProjectManager
 }
 
 func NewFleetManager(cnf *config.Config, cmd *cobra.Command) *FleetManager {
-	return &FleetManager{
+	fleetManager := &FleetManager{
 		config:  cnf,
 		command: cmd,
 	}
+
+	fleetManager.Project = NewProjectManager(fleetManager)
+
+	return fleetManager
 }
 
 // Check the authentication status.
@@ -26,7 +38,7 @@ func NewFleetManager(cnf *config.Config, cmd *cobra.Command) *FleetManager {
 func (m *FleetManager) Authentication() (bool, string, error) {
 	arguments := []string{"auth:info", "email", "--no-auto-login"}
 
-	output, err := m.getExecOutput(arguments)
+	output, err := m.GetExecOutput(arguments)
 	if err != nil {
 		return false, "", err
 	}
@@ -40,19 +52,21 @@ func (m *FleetManager) Authentication() (bool, string, error) {
 	return true, string(stripped), nil
 }
 
-func (m *FleetManager) getExecOutput(arguments []string) (string, error) {
+func (m *FleetManager) Exec(arguments []string) error {
+	wrapper := m.createCliWrapperWithDefaultIO()
+	if err := wrapper.Exec(m.command.Context(), arguments...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *FleetManager) GetExecOutput(arguments []string) (string, error) {
 	// buffers inmemory
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 
-	wrapper := &legacy.CLIWrapper{
-		Config:             m.config,
-		Version:            config.Version,
-		DisableInteraction: true,
-		Stdout:             &stdout,
-		Stderr:             &stderr,
-		Stdin:              nil,
-	}
+	wrapper := m.createCliWrapper(&stdout, &stderr, nil)
 
 	if err := wrapper.Exec(m.command.Context(), arguments...); err != nil {
 		newError := err
@@ -68,19 +82,128 @@ func (m *FleetManager) getExecOutput(arguments []string) (string, error) {
 	return stdout.String(), nil
 }
 
-func (m *FleetManager) Exec(arguments []string) error {
-	wrapper := &legacy.CLIWrapper{
+func (m *FleetManager) createCliWrapperWithDefaultIO() *legacy.CLIWrapper {
+	return m.createCliWrapper(m.command.OutOrStdout(), m.command.ErrOrStderr(), m.command.InOrStdin())
+}
+
+func (m *FleetManager) createCliWrapper(stdout io.Writer, stderr io.Writer, stdin io.Reader) *legacy.CLIWrapper {
+	return &legacy.CLIWrapper{
 		Config:             m.config,
 		Version:            config.Version,
 		DisableInteraction: true,
-		Stdout:             m.command.OutOrStdout(),
-		Stderr:             m.command.ErrOrStderr(),
-		Stdin:              m.command.InOrStdin(),
+		Stdout:             stdout,
+		Stderr:             stderr,
+		Stdin:              stdin,
+	}
+}
+
+//
+// ProjectManager
+//
+
+type ProjectInfo struct {
+	ProjectID         string
+	ProjectTitle      string
+	Region            string
+	OrganizationName  string
+	OrganizationID    string
+	OrganizationLabel string
+	OrganizationType  string
+	Status            string
+	Created           string
+}
+
+type ProjectManager struct {
+	fleetManager *FleetManager
+}
+
+func NewProjectManager(fleetManager *FleetManager) *ProjectManager {
+	return &ProjectManager{
+		fleetManager: fleetManager,
+	}
+}
+
+func (pm *ProjectManager) List() ([]ProjectInfo, error) {
+	result := []ProjectInfo{}
+	args := []string{"project:list", "--format=csv", "--count=0", "--columns=*"}
+	data, err := pm.fleetManager.GetExecOutput(args)
+
+	if err != nil {
+		return nil, err
 	}
 
-	if err := wrapper.Exec(m.command.Context(), arguments...); err != nil {
-		return err
+	reader := csv.NewReader(bytes.NewReader([]byte(data)))
+	headers, err := reader.Read()
+	if err != nil {
+		return nil, err
+	}
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
 	}
 
-	return nil
+	IDPos := -1
+	TitlePos := -1
+	RegionPos := -1
+	OrganizationNamePos := -1
+	OrganizationIDPos := -1
+	OrganizationLabelPos := -1
+	OrganizationTypePos := -1
+	StatusPos := -1
+	CreatedPos := -1
+
+	for i, header := range headers {
+		switch header {
+		case "ID":
+			IDPos = i
+		case "Title":
+			TitlePos = i
+		case "Region":
+			RegionPos = i
+		case "Org name":
+			OrganizationNamePos = i
+		case "Org ID":
+			OrganizationIDPos = i
+		case "Org label":
+			OrganizationLabelPos = i
+		case "Org type":
+			OrganizationTypePos = i
+		case "Status":
+			StatusPos = i
+		case "Created":
+			CreatedPos = i
+		}
+	}
+	for _, record := range records {
+		project := ProjectInfo{}
+		if IDPos >= 0 && IDPos < len(record) {
+			project.ProjectID = record[IDPos]
+		}
+		if TitlePos >= 0 && TitlePos < len(record) {
+			project.ProjectTitle = record[TitlePos]
+		}
+		if RegionPos >= 0 && RegionPos < len(record) {
+			project.Region = record[RegionPos]
+		}
+		if OrganizationNamePos >= 0 && OrganizationNamePos < len(record) {
+			project.OrganizationName = record[OrganizationNamePos]
+		}
+		if OrganizationIDPos >= 0 && OrganizationIDPos < len(record) {
+			project.OrganizationID = record[OrganizationIDPos]
+		}
+		if OrganizationLabelPos >= 0 && OrganizationLabelPos < len(record) {
+			project.OrganizationLabel = record[OrganizationLabelPos]
+		}
+		if OrganizationTypePos >= 0 && OrganizationTypePos < len(record) {
+			project.OrganizationType = record[OrganizationTypePos]
+		}
+		if StatusPos >= 0 && StatusPos < len(record) {
+			project.Status = record[StatusPos]
+		}
+		if CreatedPos >= 0 && CreatedPos < len(record) {
+			project.Created = record[CreatedPos]
+		}
+		result = append(result, project)
+	}
+	return result, nil
 }
